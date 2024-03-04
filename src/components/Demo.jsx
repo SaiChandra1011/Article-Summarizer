a90413c45f3d5b629d7eddecb5de7eaa48137837
SaiChandra1011 <saichandra1001@gmail.com>
2024-03-04 20:18:40 +0530

    display summary

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -5,12 +5,29 @@ import { useLazyGetSummaryQuery } from '../services/article';
 
 
 const Demo = () => {
-  const [article, setfarticle] = useState({
+  const [article, setArticle] = useState({
     url:"",
     summary:"",
   });
 
+const [allArticles, setAllArticles] = useState([]);
+
+const [copied, setCopied] = useState("");
+
 const [getSummary, {error,isFetching}] = useLazyGetSummaryQuery();
+
+
+
+//storing aritcles in local storage
+useEffect(() => {
+  const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
+
+  if(articlesFromLocalStorage){
+    setAllArticles(articlesFromLocalStorage);
+  }
+}, []);
+
+
 
   const handleSubmit =  async (e) => {
     e.preventDefault();
@@ -20,10 +37,24 @@ const [getSummary, {error,isFetching}] = useLazyGetSummaryQuery();
   });
 
   if(data?.summary) {
-    const newArticle = { ... article,summary:data.summary}
+    const newArticle = { ... article,summary:data.summary};
+
+    const updatedAllArticles = [newArticle, ...allArticles];
 
     setArticle(newArticle);
+    setAllArticles(updatedAllArticles);
+
+    localStorage.setItem('articles', JSON.stringify
+    (updatedAllArticles));
   }
+  };
+
+  const handleCopy = (copyUrl) => {
+
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+
   }
   return (
     <section className="mt-16 w-full max-w-xl" > 
@@ -38,7 +69,7 @@ const [getSummary, {error,isFetching}] = useLazyGetSummaryQuery();
       type="url"
       placeholder='Enter the URL'
       value={article.url}
-      onChange={(e) => setfarticle({...
+      onChange={(e) => setArticle({...
       article, url:e.target.value})}
       required
       className="url_input peer"
@@ -54,12 +85,63 @@ const [getSummary, {error,isFetching}] = useLazyGetSummaryQuery();
 
       </form>
 
+    <div className="flex flex-col gap-1.5 max-h-60 overflow-y-auto" >
+      {allArticles.map((item, index) => (
+        <div
+        key ={`link-${index}`}
+        onClick={() => setArticle(item)}
+        className="link_card" 
+        >
+          <div className="copy_btn" onClick={() => {
+            handleCopy(item.url)
+          }}>
+            <img 
+            src={copied === item.url ? tick : copy}
+            alt="copy_icon"
+            className="w-[40%] h-[40%] object-contain"
+            />
+          </div>
+          <p className ="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate" >
+            {item.url}
+          </p>
+        </div>
+
+      ))}
 
 
     </div>
 
+    </div>
+    
+    <div className="my-10 max-w-full flex justify-center items-center">
+      {isFetching ? (
+        <img src={loader} alt="load" className="w-20 h-20 object-contain"  />
+      ): error ? (
+      <p className="font-inter font-bold text-black text-center">
+       Ooops...
+       <br />
+       <span className="font-satoshi font-normal text-gray-600">
+        {error?.data?.error}
+       </span>
+      </p>
+      ) : (
+        article.summary && (
+          <div className="flex flex-col gap-3">
+          <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+            Article <span className= "blue_grdient">Summary</span>
+          </h2>
+          <div className="summary_box">
+          <p className="font-inter font-medium text-sm text-gray-700">
+            {article.summary}
+          </p>
+          </div>
+          </div>
+        )
+      )}
+
+    </div>
     </section>
   )
 }
 
-export default Demo
+export default Demo;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,36 +14,10 @@ const Hero = () => {
     </div>
   </nav>
   <h1 className="head_text">Summarize Articles with <br className="max-md:hidden" /> <span className="dark_blue_gradient" >OpenAI GPT-4</span></h1>
-  <h2 className="description pt-8">SummarAI leverages the cutting-edge OpenAI GPT-4 technology to provide concise and insightful summaries of articles. At SummarAI, users encounter a seamless experience, effortlessly grasping the essence of lengthy articles with just a few clicks. Experience the future of article summarization with SummarAI.</h2>
+  <h2 className="description pt-8">SummarAI leverages the cutting-edge OpenAI GPT-4 technology to provide concise and insightful summaries of articles. SummarAI gives users  a seamless experience, effortlessly grasping the essence of lengthy articles with just a few clicks. Experience the future of article summarization with SummarAI.</h2>
 </header>
 
   )
 }
 
 export default Hero
-/*
-<header className="w-full flex justify-center items-center flex-col">
-<nav className='flex justify-between items-center w-full mb-28 pt-2'>
-  <img src ={logo1} alt="SummarAI logo"
-  className="w-72 object-contain"/>
-
-  <button type="button" onClick={() => window.open("https://github.com/SaiChandra1011/Article-Summarizer")} className="black_btn" >
-    GitHub Code
-    </button>
-  <button type="button" onClick={() => window.open("https://github.com/SaiChandra1011/Article-Summarizer")} className="black_btn" >
-    GitHub Code
-    </button>
-  <button type="button" onClick={() => window.open("https://github.com/SaiChandra1011/Article-Summarizer")} className="black_btn" >
-    GitHub Code
-    </button>
-
-</nav>
-<h1 className="head_text">
-  Summarize Articles with <br className="max-md:hidden"  />
-  <span className="dark_blue_gradient" >OpenAI GPT-4</span>
-</h1>
-<h2 className="description" >
-SummarAI leverages the cutting-edge OpenAI GPT-4 technology to provide concise and insightful summaries of articles.At SummarAI, users encounter a seamless experience, effortlessly grasping the essence of lengthy articles with just a few clicks. Experience the future of article summarization with SummarAI. 
-
-</h2>
-</header>*/
